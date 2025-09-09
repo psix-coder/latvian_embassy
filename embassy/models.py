3a48f929d1299953029548d90c81cf88be84929b
Dav2dAnderson <dav2danderson@gmail.com>
2025-09-09 23:10:22 +0500

    added: Service and Meeting models

---
+++ embassy/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.text import slugify
+
+from accounts.models import CustomUser
+# Create your models here.
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+    
+
+class Meeting(models.Model):
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    meet_time = models.DateTimeField()
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    def __str__(self):
+        pass
+
+
+    
